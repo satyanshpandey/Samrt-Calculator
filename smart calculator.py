1a8948d40c2d64e7a39b88d5ae5b66116da421c3
Deepak Tiwari <42883159+deepak3698@users.noreply.github.com>
2019-11-12 16:52:27 +0530

    updated
    just added a way of programming.

--- smart calculator.py
+++ smart calculator.py
@@ -1,12 +1,11 @@
-Sum=["sum","add","adding","summation","jod","+","addition","plus","dhan"]
 
-subtract=["sub","ghatana","subtract","-","subtraction","minus","rin","ghata"]
+Sum=["sum","add","adding","summation","jod","+","addition"]
+
+subtract=["sub","ghatana","subtract","-"]
 
 division = ["/'","bhag","devide","devided","devision","half","upon","div"]
 
 multiplication = ["multiply","guna","gune","*","into"]
-
-HCF = ["hcf","HCF","highest common factor"]
 
 def val_frm_txt(text):
     val = []
@@ -17,35 +16,62 @@ def val_frm_txt(text):
         except Exception:
             char.append(ch)
     return val,char
-input_val=input("Enter what you want !")
-a,b=val_frm_txt(input_val)
-for ch in b:
-    if(ch.lower() in Sum):
-        add=0
-        for i in a:
-            add=add + i
-        print(add)
-        break
-    elif(ch.lower() in subtract):
-        sub = a[0]
-        a[0]=0
-        for i in a:
-            sub = sub - i
-        print(sub)
-        break
-    elif(ch.lower() in division):
-        div = a[0]
-        a[0] = 1
-        for i in a:
-            div = div/i
-        print(div)
-        break
-    elif(ch.lower() in multiplication):
-        mul = 1
-        for i in a:
-            mul=mul * i
-        print(mul)
-        break
-else:
-    print("Sorry we could not find what you want from these expressions")
-input()
+
+def sum(list1):
+    add = 0
+    for i in list1:
+        add = add + i
+    return add
+
+def sub1(list1):
+    sub = list1[0]
+    del list1[0]
+    for i in list1:
+        sub = sub - i
+    return sub
+
+def div1(list1):
+    div = list1[0]
+    list1[0] = 1
+    for i in list1:
+        div = div / i
+    return div
+def mul1(list1):
+    mul = 1
+    for i in list1:
+        mul = mul * i
+    return mul
+
+
+input_yes_no="yes"
+
+while(input_yes_no == "yes"):
+    input_val = input("Enter what you want !")
+    val, operation = val_frm_txt(input_val)
+
+    for ch in operation:
+        if(ch.lower() in Sum):
+            sum1=sum(val)
+            print(f"The Addition of all the number's are {sum1}")
+            break
+
+        elif(ch.lower() in subtract):
+            sub=sub1(val)
+            print(f"The Subtraction of all the given number's are {sub}")
+            break
+
+        elif (ch.lower() in division):
+            div=div1(val)
+            print(f"The Division of the given number's are {div}")
+            break
+
+        elif (ch.lower() in multiplication):
+            mul = mul1(val)
+            print(f"The Multiplication of the given number's are {mul}")
+            break
+    else:
+        print(" Sorry we could not find what you want from these expressions ")
+
+    input_yes_no = input("Do you want continue::yes/no ")
+
+print("!!!!!!!  Thanks for Using this Smart Calculator  !!!!!!!")
